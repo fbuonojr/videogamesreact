b558cf4b4a49fdc7493c610a3218beff87c918cf
Frank Buono <fbuonojr@fordham.edu>
2021-01-19 14:33:44 -0500

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './components/Navbar';
+
+function App() {
+  return (
+    <div className="container">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+function Navigation() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>FRANK</Navbar.Brand>
+      </Navbar>
+    </>
+  );
+}
+
+export default Navigation;
