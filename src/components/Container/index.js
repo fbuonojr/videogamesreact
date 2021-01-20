a27002e1cf8c2d9f6a991b5d828069214521425e
Frank Buono <fbuonojr@fordham.edu>
2021-01-20 16:04:22 -0500

    basic page layout setup

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import Row from '../Row/index';
-import Col from '../Column/index';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Contain() {
     return (
         <Container fluid>
-            <Row>
+            <Row className="text-center">
                 <Col>1 of 3</Col>
                 <Col>2 of 3</Col>
                 <Col>3 of 3</Col>
