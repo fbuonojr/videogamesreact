55a7751e3465d15826c744b6935ca8e51cd73a2e
Frank Buono <fbuonojr@fordham.edu>
2021-01-20 15:39:36 -0500

    container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from './components/Navbar';
+import Container from './components/Container';
 
 function App() {
   return (
     <div>
       <Navigation/>
+      <Container/>
     </div>
   );
 }
---
+++ src/components/Container/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Contain(){
+    return (
+    <Container fluid></Container>
+    );
+}
+
+export default Contain;
